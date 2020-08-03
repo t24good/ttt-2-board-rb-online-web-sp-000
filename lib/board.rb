45a76438062af9fba3c45733eea0ed992c0a7b5f
Tanesha Goodman <t24good@icloud.com>
2020-08-03 13:50:25 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,2 @@
-# Define the variable board below.
-def board = ["0", "1", "2", "3", "4", "5", "6", "7". "8"]
+# Define the variable board bel board = ["0", "1", "2", "3", "4", "5", "6", "7". "8"]
 end
